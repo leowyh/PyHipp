87bcfed7552a7254b49f2c7502575be5911633af
AWS ParallelCluster user <ec2-user@ip-10-0-13-224.ap-southeast-1.compute.internal>
2021-11-02 09:56:51 +0000

    updated rplhighpass-sort-slurm.sh to use separate conda environments

--- rplhighpass-sort-slurm.sh
+++ rplhighpass-sort-slurm.sh
@@ -13,6 +13,12 @@
 #SBATCH -e rplhpss-slurm.%N.%j.err # STDERR
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+/data/miniconda3/bin/conda init
+source ~/.bashrc
+envarg=`/data/src/PyHipp/envlist.py`
+conda activate $envarg
+
+
 python -u -c "import PyHipp as pyh; \
 import time; \
 pyh.RPLLFP(saveLevel=1); \
@@ -26,4 +32,7 @@ mountain_batch.mountain_batch(); \
 from PyHipp import export_mountain_cells; \
 export_mountain_cells.export_mountain_cells(); \
 print(time.localtime());"
+
+conda deactivate 
+/data/src/PyHipp/envlist.py $envarg
 
